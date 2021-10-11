8f819921eefd1bacd3f93911097d31a9c44a9384
ЪЕЪ <@DESKTOP-IL0940O>
2021-10-11 17:16:53 +0600

    Добавьте файлы проекта.

---
+++ Tours/AddEditPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Tours
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Hotel _currenthotel = new Hotel();
+
+        public AddEditPage()
+        {
+            InitializeComponent();
+            DataContext = _currenthotel;
+            ComboCountries.ItemsSource = TourBase319Entities.GetContext().Country.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currenthotel.Name))
+                errors.AppendLine("Укажите название отеля");
+            if (_currenthotel.CountryOfStars < 1 || _currenthotel.CountryOfStars > 5)
+                errors.AppendLine("Количество звезд - число от 1 до 5");
+        }
+    }
+}
---
+++ Tours/HotelPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tours
+{
+    /// <summary>
+    /// Логика взаимодействия для HotelPage.xaml
+    /// </summary>
+    public partial class HotelPage : Page
+    {
+        public HotelPage()
+        {
+            InitializeComponent();
+            DGHotel.ItemsSource = TourBase319Entities.GetContext().Hotel.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
